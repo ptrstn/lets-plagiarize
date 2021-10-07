8051a0a47b8efca16546e5323d7ed4680e1d8ed0
Peter Stein <peterstein@protonmail.com>
2021-10-07 04:19:03 +0200

    Plagiarize through multiple bypass languages

--- plagiarize/core.py
+++ plagiarize/core.py
@@ -1,6 +1,15 @@
 import deepl
 
 
-def plagiarize(text, source_language="EN", bypass_language="DE"):
-    translated_text = deepl.translate(source_language, bypass_language, text)
-    return deepl.translate(bypass_language, source_language, translated_text)
+def plagiarize(text, source_language="EN", bypass_languages=["DE", "FR", "RU", "ES"]):
+    current_language = source_language
+    next_language = bypass_languages[0]
+    bypassed_text = text
+
+    for index, bypass_language in enumerate(bypass_languages):
+        bypassed_text = deepl.translate(current_language, next_language, bypassed_text)
+        if index + 1 < len(bypass_languages):
+            current_language = next_language
+            next_language = bypass_languages[index + 1]
+
+    return deepl.translate(bypass_languages[-1], source_language, bypassed_text)
